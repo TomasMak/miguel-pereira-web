90d7f1740ce27c9522b522690522f0194b46547b
Tomas M <t.makauskas@gmail.com>
2019-03-25 22:43:05 +0100

    Final changes

--- js/custom.js
+++ js/custom.js
@@ -20,16 +20,20 @@ let projectIndexes = {
   project3: 1,
   project4: 1,
   project5: 1,
-  // project6: 1,
+  // project6: 1,   // Remove // from the beginning of line to unlock it
   // project7: 1,
+  // project5: 1,
+  // project5: 1,
 }
 showDivs("project1", projectIndexes.project1);
 showDivs("project2", projectIndexes.project2);
 showDivs("project3", projectIndexes.project3);
 showDivs("project4", projectIndexes.project4);
 showDivs("project5", projectIndexes.project5);
-// showDivs("project6", projectIndexes.project6);
+// showDivs("project6", projectIndexes.project6);   // Remove // from the beginning of line to unlock it
 // showDivs("project7", projectIndexes.project7);
+// showDivs("project8", projectIndexes.project8);
+// showDivs("project9", projectIndexes.project9);
 
 // Image library 
 function plusDivs(project, n) {
